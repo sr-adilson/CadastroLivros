8417ff46e53b4aff30f5ccf6b438de9b2bc896ae
Adilson <sr.adilson@gmail.com>
2021-08-11 10:54:22 -0300

    projeto Vini e Adilson

---
+++ LivroLibrary/LivroLibrary/Context/BaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivroLibrary.Context
+{
+    class BaseContext: DbContext
+    {
+        public DbSet<Biblioteca> Biblioteca { get; set; }
+        public DbSet<Livraria> Livraria { get; set; }
+        public BaseContext(): base("DbConectionLibrary")
+        {
+
+        }
+    }
+}
